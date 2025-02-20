6f8c24e27c93822b6fc75ddf3e254ee74c662814
beamay <73499114+beamay@users.noreply.github.com>
2025-02-20 12:49:17 -0500

    Levels done
    I added the data in json as indicated (map with all the elements - player, monsters, keys etc.., as well as the other data - time, chip count, etc). Generated the dat file from part_3_convert and tested the game a few times.

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -1,21 +1,17 @@
-import cc_classes
-import cc_dat_utils
 import json
+import cc_dat_utils
 
 #Part 3
 #Load your custom JSON file
-json_data = json.load(open("data/bea_test_part3.json", 'r'))
-print(json_data)
+with open("data/bea_test_part3.json", "r") as file:
+    json_data = json.load(file)
+
 #Convert JSON data to CCLevelPack
-pack = cc_classes.CCLevelPack()
-for level_json in json_data["levels"]:
-    level = cc_classes.CCLevel()
-    level.level_number = level_json["level_number"]
-    #level.time = level_json["time"]
-    #level.num_chips = level_json["num_chips"]
-    #level.upper_layer = level_json["upper_layer"]
-    #level.lower_layer = level_json["lower_layer"]
-    #level.optional_fields = level_json["optional_fields"]
-    pack.add_level(level)
-print(pack)
+cc_level_pack = cc_dat_utils.make_cc_level_pack_from_json(json_data)
+
 #Save converted data to DAT file
+print(cc_level_pack)
+output_dat_file = "data/bea_test_part3.dat"
+cc_dat_utils.write_cc_level_pack_to_dat(cc_level_pack, output_dat_file)
+
+print("Conversion complete! Saved as {output_dat_file}")
